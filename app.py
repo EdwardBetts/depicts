1a7872b136bbaad24b2df2b7b096e6ecec52fe15
Edward Betts <edward@4angle.com>
2019-10-14 20:04:04 +0100

    really fix.

--- app.py
+++ app.py
@@ -316,11 +316,13 @@ def existing_depicts_from_entity(entity):
 
 def get_institution(entity, other):
     if 'P276' in entity['claims']:
-        location = wikibase.first_datavalue(entity, 'P276')['id']
-        return other[location]
-    elif 'P195' in entity['claims']:
-        collection = wikibase.first_datavalue(entity, 'P195')['id']
-        return other[collection]
+        location = wikibase.first_datavalue(entity, 'P276')
+        if location:
+            return other[location['id']]
+    if 'P195' in entity['claims']:
+        collection = wikibase.first_datavalue(entity, 'P195')
+        if collection:
+            return other[collection['id']]
 
 @app.route("/item/Q<int:item_id>")
 def item_page(item_id):
