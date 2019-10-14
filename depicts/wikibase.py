8a90bf5e3fbf19b122889986f03b2a8e5e2b552f
Edward Betts <edward@4angle.com>
2019-10-14 19:59:58 +0100

    bug fix

--- depicts/wikibase.py
+++ depicts/wikibase.py
@@ -1,6 +1,8 @@
 def first_datavalue(entity, pid):
     if pid in entity['claims']:
-        return entity['claims'][pid][0]['mainsnak']['datavalue']['value']
+        mainsnak = entity['claims'][pid][0]['mainsnak']
+        if 'datavalue' in mainsnak:
+            return mainsnak['datavalue']['value']
 
 def get_entity_label(entity):
     if 'en' in entity['labels']:
