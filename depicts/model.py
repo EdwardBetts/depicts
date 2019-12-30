f1264c833391bbb84890105ea249f5d56339d5ae
Edward Betts <edward@4angle.com>
2019-12-30 09:20:53 +0000

    Remove unused fields for depicts.

--- depicts/model.py
+++ depicts/model.py
@@ -26,9 +26,7 @@ class DepictsItem(Base):
     item_id = Column(Integer, primary_key=True, autoincrement=False)
     label = Column(String)
     description = Column(String)
-    commons = Column(String)
     count = Column(Integer)
-    entity = Column(postgresql.JSON)
     qid = column_property('Q' + cast(item_id, String))
     db_alt_labels = relationship('DepictsItemAltLabel',
                                  collection_class=set,
@@ -95,7 +93,6 @@ class Item(Base):
         return [v['mainsnak']['datavalue']['value']['id']
                 for v in values
                 if 'datavalue' in v['mainsnak']]
-
 
     @property
     def date(self):
